961867f6113e49b913d7ef87cc615a89caabfbc1
Ifeanyi O <ifemmak@outlook.com>
2023-02-20 19:42:59 -0500

    kodekloud engineer

--- kodekloud01.sh
+++ kodekloud01.sh
@@ -2,7 +2,7 @@
 
 ssh username@servername                                                         #login to server
 Add server password                                                             # Add sever password to gain access
-sudo useradd -s /bin/false no login or sudo useradd -s /usr/sbin/nologin        # Add the new username
+sudo useradd -s /bin/false james or sudo useradd -s /usr/sbin/nologin james     # Add the new username
 cat /etc/passwd                                                                 # To confirm username added
 sudo userdel username                                                           # To delete username
 
